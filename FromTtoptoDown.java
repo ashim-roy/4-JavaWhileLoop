80e69d6b211f034b10b02fc6e5c71cc122bea276
Ashim <ashim.roy2@hotmail.com>
2022-11-30 21:40:11 +0530

    commiting top2down

---
+++ FromTtoptoDown.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+import java.util.*;
+//Write a program to print all Natural numbers from 1 to N where you have to take N as input from user
+public class FromTtoptoDown {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int i =1;
+        while(N>=i){
+            System.out.print(i);
+            System.out.print(" ");
+            i = i+1;
+        }
+    }
+
+}
+
+
